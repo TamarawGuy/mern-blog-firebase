cf41f705f9d8e8950f132be1e87f5b85651aa0aa
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-01-13 18:47:54 +0200

    Add routes for pages and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
