6007b7be11bf66c9674c314dfc40345b8be0e2a5
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-01-14 18:21:08 +0200

    Add data loading with axios for article page

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
 import NotFoundPage from "./NotFoundPage";
 import articles from "../data/articles-data";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
   const { articleId } = useParams();
   const article = articles.find((article) => article.name === articleId);
+
+  useEffect(() => {
+    const loadInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+
+    loadInfo();
+  }, []); // eslint-disable-line
 
   if (!article) {
     return <NotFoundPage />;
@@ -12,6 +27,7 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvotes!</p>
       {article.content.map((par, index) => (
         <p key={`${index}-${par}`}>{par}</p>
       ))}
