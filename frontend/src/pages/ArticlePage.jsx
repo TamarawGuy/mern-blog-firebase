37395193c5ae89370cddef7ab017a666930b71cf
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-01-14 19:04:17 +0200

    Add comment section form

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,38 +1,51 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
 import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
-
+import AddCommentForm from "../components/AddCommentForm";
 import articles from "../data/articles-data";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-
   const { articleId } = useParams();
-  const article = articles.find((article) => article.name === articleId);
 
   useEffect(() => {
-    const loadInfo = async () => {
+    const loadArticleInfo = async () => {
       const response = await axios.get(`/api/articles/${articleId}`);
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     };
 
-    loadInfo();
+    loadArticleInfo();
   }, []); // eslint-disable-line
+
+  const article = articles.find((article) => article.name === articleId);
+
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
+  };
 
   if (!article) {
     return <NotFoundPage />;
   }
+
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvotes!</p>
-      {article.content.map((par, index) => (
-        <p key={`${index}-${par}`}>{par}</p>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
