56e4ef709ebc90fa0dd533d7b746875560b29731
TamarawGuy <viktor.m.rumenov@gmail.com>
2023-01-14 18:04:03 +0200

    Rewrite post and put method for articles to get and modify data from database

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,29 +16,41 @@ app.get("/api/articles/:name", async (req, res) => {
   }
 });
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.put("/api/articles/:name/upvote", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((article) => article.name === name);
+
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    res.send(`${name} article now has ${article.upvotes} upvotes!`);
   } else {
     res.send("Article does not exist");
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
 
-  const article = articlesInfo.find((article) => article.name === name);
+  await db.collection("article").updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy, text } },
+    }
+  );
+
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
     res.send(article.comments);
   } else {
-    res.send("Article does not exist!");
+    res.send("Article does not exist");
   }
 });
 
